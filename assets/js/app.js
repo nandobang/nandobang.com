16e9d336dccd62164b39dae75689a49e759f6078
nandobang <lfbang87@gmail.com>
2015-10-28 19:46:15 -0200

    Initial version.

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+(function ($) {
+  console.log('App bootstraped!');
+
+  $(document).on('ready', function () {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+        var target = $(this.hash);
+
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+
+          return false;
+        }
+      }
+    });
+
+    $('.menu .expand-menu').on('click', function (evt) {
+      $('.menu').toggleClass('expanded');
+    });
+
+    $('.menu .links a').on('click', function (evt) {
+      $('.menu').toggleClass('expanded');
+    });
+  });
+})(jQuery);
